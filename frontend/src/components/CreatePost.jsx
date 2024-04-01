84e8ad2a282be5c91b348930260b748bbdd22c4c
Priyanka <priyankashah3107@gmail.com>
2024-04-02 00:12:07 +0530

    feat: ReactQuill Editior

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function CreatePost() {
+  const [value, setValue] = useState('');
+
+  const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    ['link', 'image', 'video', 'formula'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
+  
+  // const quill = new Quill('#editor', {
+  //   modules: {
+  //     toolbar: toolbarOptions
+  //   },
+  //   theme: 'snow'
+  // });
+
+  const module = {
+    toolbar: toolbarOptions
+  }
   return (
-    <div>CreatePost</div>
+    <>  
+    <div  className='box-border'>
+    <ReactQuill theme="snow" value={value} onChange={setValue}  modules={module}/>;
+    </div>
+      
+     
+
+    </>
   )
 }
 
