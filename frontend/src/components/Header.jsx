412d0dadb58cc0677b50a4911bae54afe490c62f
Priyanka <priyankashah3107@gmail.com>
2024-04-01 17:20:43 +0530

    feat: CreatePost + Logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
+  const [username, setUsername] = useState(null)
   useEffect(()=>  {
     fetch("http://localhost:3333/profile", {
       credentials: "include"
-    });
+    }).then(response => {
+       response.json().then(userInfo => {
+         setUsername(userInfo.username)
+       })
+    })
   }, []);
 
   return (
     <header className="flex justify-between m-8 text-black items-center ">
       <Link to={"/"} className="text-2xl font-bold">BlogVista</Link>
       <div className="flex justify-between gap-10 font-bold ">
-        <Link to={"/login"} >Login</Link>
+       {username && (
+        <> 
+        <Link to={'/create'}>Create Blog</Link>
+        <a href="">Logout</a>
+        </>
+       )}
+       {!username && (
+        <>
+         <Link to={"/login"} >Login</Link>
         <Link to={"/signup"}>Signup</Link>
+        </>
+       )}
+       
       </div>
     </header>
   )
