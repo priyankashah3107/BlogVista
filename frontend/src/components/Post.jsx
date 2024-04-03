7f096ed8fe8087851cea93936d386f70b3fc7528
Priyanka <priyankashah3107@gmail.com>
2024-04-03 18:36:38 +0530

    IMG ISSUE

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -26,6 +26,7 @@
 
 import React from 'react';
 import { format } from 'date-fns';
+import { Link } from 'react-router-dom';
 function Post({title, metaData, value, cover, createdAt, author
 }) {
 
@@ -33,16 +34,23 @@ function Post({title, metaData, value, cover, createdAt, author
   createdAt = format(postingDate, 'MMMM do, yyyy')
   // createdAt = format(postingDate, 'yyyy-MM-dd')
   return (
-    <div className="post m-8 flex flex-col gap-5 text-start mt-20">
-      <div className="flex flex-col sm:flex-row gap-10">
-        <img src={'http://localhost:3333/'+cover} alt="image" className="w-full sm:w-1/2 rounded-lg md:w-1/2 lg:w-1/4  " />
-        <div className="content pl-4 sm:pl-0 pt-4 sm:pt-0 flex-grow">
+    <div className="post m-8 flex flex-col gap-5text-start mt-20">
+      <div className="flex flex-col sm:flex-row gap-1">
+        
+        <div className=''>
+         <Link >
+         <img src={'http://localhost:3333/'+cover} alt="image" className="w-[750px] h-[200px] rounded-lg   " />
+
+         </Link>
+        </div>
+
+        <div className=" pl-4 sm:pl-0 pt-4 sm:pt-0 flex flex-col ml-10 ">
           <h2 className="font-bold text-4xl">{title}</h2>
           <div className="flex gap-3 mt-3">
             <h5 className="font-bold">{author.username}</h5>
             <time>{createdAt}</time>
           </div>
-          <p className="mt-6 text-2xl">
+          <p className="mt-6 text-xl">
            {metaData}
           </p>
         </div>
